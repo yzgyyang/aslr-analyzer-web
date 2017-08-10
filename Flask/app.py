447d90ffb2d651c2a87c94bebe664214af896068
Guangyuan Yang <yzgyyang@outlook.com>
2017-08-10 04:06:52 -0400

    Add result.html and related templates

--- Flask/app.py
+++ Flask/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/result')
+@app.route('/result/<os_name>')
+def results(os_name="freebsd"):
+    return render_template("result.html", os_name=os_name)
