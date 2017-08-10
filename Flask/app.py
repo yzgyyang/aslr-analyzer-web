a0dee607af136f1a64145054db2e60e4d6279339
Guangyuan Yang <yzgyyang@outlook.com>
2017-08-10 12:12:38 -0400

    Add basic file uploading function

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,16 @@
+import os
 from flask import Flask, render_template, request, url_for
+from werkzeug.utils import secure_filename
 
 # Initialize the Flask application
 app = Flask(__name__)
+
+# Configs
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['txt', 'out'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024
 
 # Constants
 NAVS = [
@@ -28,7 +37,25 @@ def index():
 
 @app.route('/new')
 def new():
-    pass
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded', filename=filename))
+    return '''
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/new/<filename>')
+def uploaded(filename):
+    return filename
 
 @app.route('/results')
 @app.route('/results/<os_name>')
@@ -37,3 +64,8 @@ def results(os_name="freebsd"):
         os_name=os_name,
         tested_os=TESTED_OS,
         navs=NAVS)
+
+
+# Helpers
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
