88688b88e288c63e1160fa820a6741b5d78e3f64
Guangyuan Yang <yzgyyang@outlook.com>
2017-08-10 04:29:15 -0400

    Add auto-loading to result_content.html

--- Flask/app.py
+++ Flask/app.py
@@ -3,11 +3,23 @@ from flask import Flask, render_template, request, url_for
 # Initialize the Flask application
 app = Flask(__name__)
 
+# Constants
+TESTED_OS = [
+    ('freebsd', 'FreeBSD 12-CURRENT with ASLR Patch'),
+    ('openbsd', 'OpenBSD 6.1'),
+    ('netbsd', 'NetBSD 7.1'),
+    ('dragonfly', 'DragonFly BSD 4.8.1'),
+    ('macos', 'macOS 10.12'),
+    ('ubuntu', 'Ubuntu 17.04'),
+    ('arch', 'Arch Linux 201708')
+]
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/result')
-@app.route('/result/<os_name>')
+@app.route('/results')
+@app.route('/results/<os_name>')
 def results(os_name="freebsd"):
-    return render_template("result.html", os_name=os_name)
+    return render_template("result.html",
+        os_name=os_name, tested_os=TESTED_OS)
