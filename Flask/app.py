a849fff97456591cce34d7f2f06b480b70c8e3a1
Guangyuan Yang <yzgyyang@outlook.com>
2017-08-10 05:02:10 -0400

    Add auto-loading to navbar

--- Flask/app.py
+++ Flask/app.py
@@ -4,6 +4,12 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 # Constants
+NAVS = [
+    ('index', 'Home'),
+    ('new', 'New'),
+    ('results', 'Results')
+]
+
 TESTED_OS = [
     ('freebsd', 'FreeBSD 12-CURRENT with ASLR Patch'),
     ('openbsd', 'OpenBSD 6.1'),
@@ -16,10 +22,18 @@ TESTED_OS = [
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",
+        nav_name="index",
+        navs=NAVS)
+
+@app.route('/new')
+def new():
+    pass
 
 @app.route('/results')
 @app.route('/results/<os_name>')
 def results(os_name="freebsd"):
     return render_template("result.html",
-        os_name=os_name, tested_os=TESTED_OS)
+        os_name=os_name,
+        tested_os=TESTED_OS,
+        navs=NAVS)
